27c21f68f929119cc9b9945a2154416d188feb39
Yamagu-shi <g2454809@cc.kyoto-su.ac.jp>
2024-12-08 02:47:44 +0900

    スライドバー実装.2

--- Lofi-infinity-player/src/pages/bgm/Bgm.tsx
+++ Lofi-infinity-player/src/pages/bgm/Bgm.tsx
@@ -1,25 +1,32 @@
-import YouTube ,{YouTubeProps} from 'react-youtube';
+import YouTube ,{YouTubeEvent, YouTubePlayer, YouTubeProps} from 'react-youtube';
 import bgmUrls from "./dummyBgms"
 import effectUrls from "./dummyEffetcts"
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import dummyBgms from './dummyBgms';
 
 const BGMs = () => {
 
+  let playedBgms = [];
+
   let effectUrl = effectUrls[0]
+
   const [bgmUrl, setBgmData] = useState(dummyBgms[Math.floor(Math.random() * bgmUrls.length)])
-  let playedBgms = []
+  const [bgmVolume, setBgmVolume] = useState(50);
+  const [effectVolume, setEffectVolume] = useState(50);
 
-  let bgmVolume = 50;
-  let effectVolume = 50;
+  const bgmRef: React.MutableRefObject<YouTubePlayer> = useRef(null);
+  const effectRef: React.MutableRefObject<YouTubePlayer> = useRef(null);
 
-  const onBgmReady : YouTubeProps["onReady"] = (event) => {
+  const onBgmReady : YouTubeProps["onReady"] = (event:YouTubeEvent) => {
 
-    event.target.playVideo()
-    console.log("BGM start!!")
+    event.target.playVideo();
+    console.log("BGM start!!");
+    event.target.setVolume(bgmVolume);
+
+    bgmRef.current = event.target;
   };
 
-  const onBgmEnd : YouTubeProps["onEnd"] = (event) => {
+  const onBgmEnd : YouTubeProps["onEnd"] = (_event) => {
 
     playedBgms.push(bgmUrl);
     console.log(playedBgms);
@@ -45,9 +52,12 @@ const BGMs = () => {
     }
   }
 
-  const onEffectReady : YouTubeProps["onReady"] = (event) => {
+  const onEffectReady : YouTubeProps["onReady"] = (event:YouTubeEvent) => {
     event.target.playVideo();
     console.log("Effect start");
+    event.target.setVolume(effectVolume);
+
+    effectRef.current = event.target;
   };
 
   const onEffectEnd : YouTubeProps["onEnd"] = (event) => {
@@ -55,15 +65,33 @@ const BGMs = () => {
     event.target.seekTo(0, true);
   }
 
+  const onBgmHandleChange = (event:React.ChangeEvent<HTMLInputElement>)  => {
+    const newVolume = parseInt(event.target.value, 10);
+    setBgmVolume(newVolume);
+
+    if (bgmRef.current){
+      bgmRef.current.setVolume(newVolume);
+    }
+  }
+
+  const onEffectHandleChange = (event:React.ChangeEvent<HTMLInputElement>)  => {
+    const newVolume = parseInt(event.target.value, 10);
+    setEffectVolume(newVolume);
+
+    if (effectRef.current){
+      effectRef.current.setVolume(newVolume);
+    }
+  }
+
   const bgmOpts = {
     height: '390',
     width: '640',
+    
     playerVars: {
       fs: 0,
       autoplay: 0,
       disablekb: 1,
-      color: "white",
-      rel: 0
+      color: "white"
     }
   }
 
@@ -75,8 +103,7 @@ const BGMs = () => {
       fs: 0,
       autoplay: 0,
       disablekb: 1,
-      color: "white",
-      rel: 0
+      color: "white"
     }
   }
 
@@ -84,19 +111,28 @@ const BGMs = () => {
     <div>
       <div className='BGMs'>
         <div className='BGM'>
-          <YouTube videoId={ bgmUrl } 
-            opts={bgmOpts} 
-            onReady={onBgmReady} 
-            onStateChange={onStateChange} 
-            onEnd={onBgmEnd}
-          />
+          <YouTube videoId={ bgmUrl } opts={bgmOpts} onReady={(e:YouTubeEvent)=>onBgmReady(e)} onStateChange={onStateChange} onEnd={onBgmEnd} />
+          <input
+          id="volume"
+          type="range"
+          min="0"
+          max="100"
+          value={bgmVolume}
+          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>onBgmHandleChange(e)}
+          style={{ width: "100%" }}
+        />
         </div>
         <div className='BGM'>
-          <YouTube videoId={ effectUrl } 
-            opts={effectOpts} 
-            onReady={onEffectReady} 
-            onEnd={onEffectEnd}
-          />
+          <YouTube videoId={ effectUrl } opts={effectOpts} onReady={onEffectReady} onEnd={onEffectEnd}/>
+          <input
+          id="volume"
+          type="range"
+          min="0"
+          max="100"
+          value={effectVolume}
+          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>onEffectHandleChange(e)}
+          style={{ width: "100%" }}
+        />
         </div>
       </div>
     </div>
